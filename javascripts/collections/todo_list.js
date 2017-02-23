de47f76889e29ae065f1bf57db82c68d837432e7
Antoine Leclercq <antoine.leclercq.49@gmail.com>
2017-02-23 11:49:24 -0500

    added collections

---
+++ javascripts/collections/todo_list.js
@@ -0,0 +1,31 @@
+var TodoList = Backbone.Collection.extend({
+  model: Todo,
+  lastId: 0,
+  storageName: 'todoList',
+  setLastId: function () {
+    var maxId;
+
+    if (this.isEmpty()) { return; }
+
+    maxId = this.max(function (model) { return model.get('id'); }).get('id');
+    this.lastId = this.lastId < maxId ? maxId : this.lastId;
+  },
+  nextId: function () {
+    this.setLastId();
+    return ++this.lastId;
+  },
+  readStorage: function () {
+    var todos = JSON.parse(localStorage.getItem(this.storageName));
+    if (todos) { this.set(todos); }
+  },
+  setStorage: function () {
+    localStorage.setItem(this.storageName, JSON.stringify(this.toJSON()));
+  },
+  initialize: function () {
+    this.readStorage();
+    this.on('change update', this.setStorage, this);
+    this.on('change update', this.sort);
+    this.comparator = 'isCompleted';
+    this.sort();
+  },
+});
